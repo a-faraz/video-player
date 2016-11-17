e80a4d260960c6103aee0c3db5e62748e46877df
a-faraz <anjumfaraz10@gmail.com>
2016-11-17 12:50:32 -0600

    (fix) rendering components through app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-const API_KEY = '****';
+import SearchBar from './components/search_bar';
+
+const API_KEY = '***';
 
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-	return <div>Hello World!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
 }
 
 // Take this component's generated HTML and put it
