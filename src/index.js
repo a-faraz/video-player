f3fc17c137807d6ff8a10b111fd3b57c42378737
a-faraz <anjumfaraz10@gmail.com>
2016-11-16 23:13:20 -0600

    (fix) creating App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // Create a new component. This component should produce
 // some HTML
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 const App = function() {
 	return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+
+ReactDOM.render(<App />);
