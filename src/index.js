e0a6347236a0f0124443706462829a5958bf875b
a-faraz <anjumfaraz10@gmail.com>
2016-11-16 23:35:57 -0600

    (fix) creating different components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-// Create a new component. This component should produce
-// some HTML
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const App = function() {
-	return <div>Hi!</div>;
+const API_KEY = '****';
+
+// Create a new component. This component should produce
+// some HTML
+const App = () => {
+	return <div>Hello World!</div>;
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
