4da04e2a0fc30abe2a340a920cc1a1c20e2e4574
a-faraz <anjumfaraz10@gmail.com>
2016-11-17 23:23:18 -0600

    (fix) searchbar component sets state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,18 +4,26 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { term: ''};
+		this.state = { term: ''}
 	}
 
 	render() {
-		return <input onChange={(event) => console.log(event.target.value)} />
+
+		return (
+			<div>
+				<input 
+				value={this.state.term}
+				onChange={(event) => this.setState({ term: event.target.value}) } />
+				{console.log("value: ", this.state.term)}
+			</div>
+		)
 	}
 
-// whenever input changes - run this function ** moved this function into line 6
-	// 	onInputChange(event) {
-	// 		console.log(event.target.value);
-	// 	}
-	// }
+// whenever input changes - run this function ** moved this function into line 6 **
+// onInputChange(event) {
+// 		console.log(event.target.value);
+// 	}
+
 }
 
 export default SearchBar;
