77d0402edba9069532e9e3332a62c2e879ef559b
a-faraz <anjumfaraz10@gmail.com>
2016-11-17 21:31:17 -0600

    (fix) make searchbar into class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-	return <input /> 
-};
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = { term: ''};
+	}
+
+	render() {
+		return <input onChange={(event) => console.log(event.target.value)} />
+	}
+
+// whenever input changes - run this function ** moved this function into line 6
+	// 	onInputChange(event) {
+	// 		console.log(event.target.value);
+	// 	}
+	// }
+}
 
 export default SearchBar;
